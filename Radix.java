933ed45c83f40efb8f5a99cd701362a47f976c90
Cyrus Cursetjee <ccursetjee@gmail.com>
2021-01-13 09:29:30 -0500

    length

--- Radix.java
+++ Radix.java
@@ -5,11 +5,21 @@ public class Radix {
 		return Character.getNumericValue(number.charAt(number.length() - col - 1));
 	}
 
+	public static int length(int n) {
+		String number = "" + Math.abs(n);
+		return number.length();
+	}
+
 	public static void main(String[] args) {
 		System.out.println(nth(123, 1));
 		System.out.println(nth(-123, 1));
 		System.out.println(nth(123, 2));
 		System.out.println(nth(123, 2));
+
+		System.out.println(length(0));
+		System.out.println(length(15));
+		System.out.println(length(-10));
+		System.out.println(length(5112));
 	}
 
 }
